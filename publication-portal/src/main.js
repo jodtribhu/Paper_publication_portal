f025b7c6886d88309231fe49f8de113e96c94af0
Jodiss Tribhu <jodiss.tri@gmail.com>
2021-09-03 10:22:00 +0530

    Login_Page

--- publication-portal/src/main.js
+++ publication-portal/src/main.js
@@ -4,10 +4,15 @@ import router from './router/router.js';
 import Formcard from './components/layout/FormCard.vue';
 import BaseDialog from './components/layout/BaseDialog.vue';
 import BaseCard from './components/layout/BaseCard.vue';
+import ForgetCard from './components/layout/ForgetCard.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+
 
 const app=createApp(App);
 app.component('form-card',Formcard);
 app.component('base-dialog',BaseDialog);
 app.component('base-card',BaseCard);
+app.component('forget-card',ForgetCard);
+app.component('the-header',TheHeader);
 app.use(router);
 app.mount('#app');
--- publication-portal/src/router/router.js
+++ publication-portal/src/router/router.js
@@ -1,12 +1,16 @@
 import {createRouter,createWebHistory} from 'vue-router';
 import StudentForm from '../components/Student/StudentForm.vue';
 import FacultyView from '../components/Faculty/FacultyView.vue';
+import Login from '../components/Authentication/Login.vue';
+import ForgotPassword from '../components/Authentication/ForgotPassword.vue';
 const router=createRouter({
     history:createWebHistory(),
     routes:[
         {path:'/',redirect:'/home'},
         {path:'/home',component:StudentForm},
         {path:'/faculty',component:FacultyView},
+        {path:'/login',component:Login},
+        {path:'/ForgotPassword',component:ForgotPassword},
     ]
 });
 export default router;
